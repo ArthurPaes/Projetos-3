459e072754d3fa487ef8c91675d1cd462280ae76
Arthur Paes <arthur.paes@helpper.com.br>
2022-05-09 00:32:56 -0300

    fix: clean up

--- src/pages/Lesson/index.tsx
+++ src/pages/Lesson/index.tsx
@@ -2,43 +2,28 @@
 /* eslint-disable react/jsx-props-no-spreading */
 /* eslint-disable no-unused-expressions */
 import React, { useCallback, useEffect, useState, useContext } from "react";
-import { useRouteMatch, Link } from "react-router-dom";
-import { FiChevronLeft } from "react-icons/fi";
-import api from "../../services/api";
 
 import Header from "../../components/Header";
 import { Button } from "@material-ui/core";
 
-import { HandwritingInput } from "../../react-handwriting-recognition";
-import keyboard from 'react-handwriting-keyboard';
-
+import keyboard from "react-handwriting-keyboard";
 
 const Lesson: React.FunctionComponent = () => {
   const [inputValue, setInputValue] = useState("");
 
-  const InputComponent =({style}:{style:any}) =>  {
-    const showKeyboard = (e:any) => {
+  const InputComponent = ({ style }: { style: any }) => {
+    const showKeyboard = (e: any) => {
       const input = e.target;
       keyboard.show({
         target: e.target,
-        onChange: (value:any) => {
-          console.log('value: ', value);
+        onChange: (value: any) => {
+          console.log("value: ", value);
           input.value = value;
         },
       });
     };
-    return <input style={style} type="text" onClick={showKeyboard} />
-  }
-
-
-  useEffect(() => {
-    console.log("inputValue", inputValue);
-  }, []);
-
-  console.log('can1')
-
-  let controls;
-
+    return <input style={style} type="text" onClick={showKeyboard} />;
+  };
 
   return (
     <>
@@ -62,34 +47,15 @@ const Lesson: React.FunctionComponent = () => {
           culpa qui officia deserunt mollit anim id est laborum.
         </div>
         <div style={{ display: "flex" }}>
-          <InputComponent style={{
-              border: "1px solid black",
-              height: "400px",
-              width: "800px",
-              marginTop: "20px",
-            }}/>
-          {/* <canvas
-            id="can"
-            width="400"
-            height="400"
-            style={{ border: "2px solid", cursor: "crosshair" }}
-          ></canvas> */}
-          {/* <HandwritingInput
-            applicationKey="b259a3a3-4ec7-4086-8d9d-d3215a89d85a"
-            hmacKey="63773195-5e31-4bfd-a593-a8f5a6a875bb"
-            protocol="WebSocket"
-            onChange={(e: any) => setInputValue(e.target.value)}
-            onError={(error: any) => console.log("erorr", error)}
-            onInit={(controls:any) => controls = controls}
-          /> */}
-          {/* <div
+          <InputComponent
             style={{
               border: "1px solid black",
               height: "400px",
               width: "800px",
               marginTop: "20px",
             }}
-          ></div> */}
+          />
+
           <div>
             <div
               style={{
